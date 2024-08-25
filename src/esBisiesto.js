900f780f2f7addd648e7172ea6e049189eec7771
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-08-25 15:21:03 -0400

    Anio divido entre 100 pero no por 400 no es bisiesto

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,11 +1,14 @@
 function esBisiesto(anio){
     let result;
-    if (anio%400 === 0){
-        result = true;
-    }
-    else{
+    if (anio%100 === 0){
         result = false;
-    }
+        if (anio%400 === 0){
+            result = true;
+        }
+        else{
+            result = false;
+        }
+    }   
     return result;
 }
-export default esBisiesto;
+   export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -5,4 +5,8 @@ describe("Es bisiesto", () => {
     const result = esBisiesto(2000)
     expect(result).toEqual(true);
   });
+  it("Todos los años divisibles por 100 pero no por 400 NO son bisiestos", () => {
+    const result = esBisiesto(1700)
+    expect(result).toEqual(false);
+  });
 });
