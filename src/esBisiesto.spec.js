6c7f4c9c629e0e0e8194246d19ae8e7fa6bb1b1a
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-08-27 09:28:15 -0400

    Corrigiendo

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -9,15 +9,11 @@ describe("Es bisiesto", () => {
     const result = esBisiesto(1700)
     expect(result).toEqual(false);
   });
-  it("Todos los años divisibles por 100 pero no por 400 NO son bisiestos", () => {
+  it("Todos los años divisibles por 4 pero no por 100 NO son bisiestos", () => {
     const result = esBisiesto(2008)
     expect(result).toEqual(true);
   });
-  it("Todos los años divisibles por 100 pero no por 400 NO son bisiestos", () => {
-    const result = esBisiesto(2008)
-    expect(result).toEqual(true);
-  });
-  it("Todos los años divisibles por 100 pero no por 400 NO son bisiestos", () => {
+  it("Todos los años no divisibles por 4 NO son bisiestos", () => {
     const result = esBisiesto(2017)
     expect(result).toEqual(false);
   });
